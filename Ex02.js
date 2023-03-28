13aee109121d4eafe26c31987e00ebdb2563b716
Mônica Ribeiro <monicaspribeiro@gmail.com>
2023-03-28 13:39:11 -0300

    Exercicios lista 1

---
+++ Ex02.js
@@ -0,0 +1,25 @@
+var nome1 = "Miguel"
+var idade1 = 22
+
+var nome2 = "Carlos "
+var idade2 = 13
+
+var nome3 = "Ana "
+var idade3 = 49
+
+var nome4 = "José "
+var idade4 = 37
+
+var nome5 = "Marcia "
+var idade5 = 71
+
+console.log("Nome:", nome1)
+console.log("Idade:", idade1, "anos")
+console.log("Nome:",nome2)
+console.log("Idade:", idade2, "anos")
+console.log("Nome:", nome3)
+console.log("Idade:", idade3, "anos")
+console.log("Nome:", nome4)
+console.log("Idade:", idade4, "anos")
+console.log("Nome:", nome5)
+console.log("Idade:", idade5, "anos")
