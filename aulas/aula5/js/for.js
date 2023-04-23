4fe9238afa76a74e7fdeaf54f8babc46f9148a51
Monica Ribeiro <monicaspribeiro@gmail.com>
2023-04-23 12:03:46 -0300

    Aula 5

---
+++ aulas/aula5/js/for.js
@@ -0,0 +1,21 @@
+var tarefas = [
+    "lavar louca",
+    "lavar roupa",
+    "fazer comida",
+    "lavar banheiro"
+]
+
+var numeros = [1,2,3,4,5,6,7,8,9,19]
+
+for(var i = 0; i < tarefas.length; i++){
+    if(tarefas[i] == "lavar banheiro"){
+        break
+    }
+    console.log(tarefas[i])
+}
+
+for(var x = 0; x < numeros.length; x++){
+    if(numeros[x] % 2 == 0){
+        
+    }
+}
