b81f75bddb2b4bf328d8b4239900e34040b01b3e
Mônica Ribeiro <monicaspribeiro@gmail.com>
2023-06-05 19:49:41 -0300

    Lista9

---
+++ aulas/Listas/Lista9/exercicios/models/lanche.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const lanchesSchema = new mongoose.Schema(
+    { nome: 
+        { type: String, 
+            required: true 
+        }, 
+        descricao: 
+        { 
+            type: String, 
+            required: true 
+        }, vegano: 
+        { type: Boolean, 
+            required: true 
+        }, 
+        emEstoque: 
+        { 
+            type: Boolean, 
+            required: true 
+        }, 
+        publicadoEm:
+        {
+            type: Date, 
+            required: true 
+        }
+    })
+
+const Lanches = mongoose.model('Lanches', lanchesSchema)
+module.exports = Lanches
